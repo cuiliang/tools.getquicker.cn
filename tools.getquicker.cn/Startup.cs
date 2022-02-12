9e1b8a63ee5a657128837e6e433dc386511d0d45
CuiLiang <cuiliang.bj@gmail.com>
2022-02-12 13:05:55 +0800

    增加filesaver

--- tools.getquicker.cn/Startup.cs
+++ tools.getquicker.cn/Startup.cs
@@ -64,7 +64,7 @@ namespace QuickerWebTools
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Quicker Web Tools v1"));
 
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseStaticFiles();
 
