2921ea16024a3403b031b16129b347a76944c2c3
CuiLiang <cuiliang.bj@gmail.com>
2022-02-14 10:59:07 +0800

    增加二维码功能。

---
+++ tools.getquicker.cn/wwwroot/static/libs/qr-scanner@1.4.0/rollup.config.js
@@ -0,0 +1,102 @@
+import alias from '@rollup/plugin-alias';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+// ts config is a combination of tsconfig.json and overrides here. Type declarations file is generated separately via
+// tsc (see build script in package.json), because rollup can not emit multiple files if using output.file option.
+import typescript from '@rollup/plugin-typescript';
+import closureCompiler from '@ampproject/rollup-plugin-closure-compiler';
+
+// not using rollup's output.banner/output.intro/output.footer/output.outro as we also have to modify the generated code
+function workerScriptToDynamicImport() {
+    return {
+        name: 'worker-script-to-dynamic-import',
+        generateBundle(options, bundle) {
+            for (const chunkName of Object.keys(bundle)) {
+                const chunk = bundle[chunkName];
+                if (chunk.type !== 'chunk') {
+                    continue;
+                }
+                chunk.code = 'export default new Worker(URL.createObjectURL(new Blob([`'
+                    + chunk.code.replace(/`/g, '\\`').replace(/\${/g, '\\${')
+                    + '`]),{type: "application/javascript"}))';
+            }
+        },
+    };
+}
+
+export default () => [
+    // worker; built first to be available for inlining in the legacy build
+    {
+        input: 'src/worker.ts',
+        output: {
+            file: 'qr-scanner-worker.min.js',
+            format: 'esm',
+            interop: false,
+            sourcemap: true,
+        },
+        plugins: [
+            typescript(),
+            sourcemaps(),
+            closureCompiler({
+                //compilation_level: 'ADVANCED',
+                //warning_level: 'QUIET',
+                language_in: 'ECMASCRIPT6',
+                language_out: 'ECMASCRIPT6',
+                rewrite_polyfills: false,
+            }),
+            workerScriptToDynamicImport(),
+        ]
+    },
+    ...([
+        // standard build specific settings
+        {
+            // Note that this results in the dynamic import of the worker to also be a dynamic import in the umd build.
+            // However, umd builds do not support multiple chunks, so that's probably the best we can do, as js dynamic
+            // imports are now widely supported anyways.
+            external: ['./qr-scanner-worker.min.js'],
+            output: [{
+                file: 'qr-scanner.min.js',
+                format: 'esm',
+            }, {
+                file: 'qr-scanner.umd.min.js',
+                format: 'umd',
+                name: 'QrScanner',
+            }],
+            language_out: 'ECMASCRIPT_2017',
+        },
+        // legacy build specific settings
+        {
+            aliases: {
+                // redirect to the built version in the dist folder
+                './qr-scanner-worker.min.js': '../qr-scanner-worker.min.js',
+            },
+            output: [{
+                file: 'qr-scanner.legacy.min.js',
+                format: 'esm',
+                // inline the worker as older browsers that already supported es6 did not support dynamic imports yet
+                inlineDynamicImports: true,
+            }],
+            language_out: 'ECMASCRIPT6',
+        },
+    ].map((specificSettings) => ({
+        input: 'src/qr-scanner.ts',
+        external: specificSettings.external,
+        output: specificSettings.output.map((output) => ({
+            interop: false,
+            sourcemap: true,
+            ...output,
+        })),
+        plugins: [
+            alias({
+                entries: specificSettings.aliases,
+            }),
+            typescript({
+                target: 'ES2017',
+            }),
+            closureCompiler({
+                language_in: 'ECMASCRIPT_2017',
+                language_out: specificSettings.language_out,
+                rewrite_polyfills: false,
+            })
+        ],
+    }))),
+];
---
+++ tools.getquicker.cn/wwwroot/static/libs/qr-scanner@1.4.0/src/worker.ts
@@ -0,0 +1,92 @@
+// @ts-ignore jsqr-es6 does not provide types currently
+import jsQR from '../node_modules/jsqr-es6/dist/jsQR.js';
+
+type GrayscaleWeights = {
+    red: number,
+    green: number,
+    blue: number,
+    useIntegerApproximation: boolean,
+};
+
+let inversionAttempts: 'dontInvert' | 'onlyInvert' | 'attemptBoth' = 'dontInvert';
+let grayscaleWeights: GrayscaleWeights = {
+    // weights for quick luma integer approximation (https://en.wikipedia.org/wiki/YUV#Full_swing_for_BT.601)
+    red: 77,
+    green: 150,
+    blue: 29,
+    useIntegerApproximation: true,
+};
+
+self.onmessage = event => {
+    const type = event['data']['type'];
+    const data = event['data']['data'];
+
+    switch (type) {
+        case 'decode':
+            decode(data);
+            break;
+        case 'grayscaleWeights':
+            setGrayscaleWeights(data);
+            break;
+        case 'inversionMode':
+            setInversionMode(data);
+            break;
+        case 'close':
+            // close after earlier messages in the event loop finished processing
+            self.close();
+            break;
+    }
+};
+
+function decode(data: { data: Uint8ClampedArray, width: number, height: number }): void {
+    const rgbaData = data['data'];
+    const width = data['width'];
+    const height = data['height'];
+    const result = jsQR(rgbaData, width, height, {
+        inversionAttempts: inversionAttempts,
+        greyScaleWeights: grayscaleWeights,
+    });
+    if (!result) {
+        (self as unknown as Worker).postMessage({
+            type: 'qrResult',
+            data: null,
+        });
+        return;
+    }
+
+    (self as unknown as Worker).postMessage({
+        type: 'qrResult',
+        data: result.data,
+        // equivalent to cornerPoints of native BarcodeDetector
+        cornerPoints: [
+            result.location.topLeftCorner,
+            result.location.topRightCorner,
+            result.location.bottomRightCorner,
+            result.location.bottomLeftCorner,
+        ],
+    });
+}
+
+function setGrayscaleWeights(data: GrayscaleWeights) {
+    // update grayscaleWeights in a closure compiler compatible fashion
+    grayscaleWeights.red = data['red'];
+    grayscaleWeights.green = data['green'];
+    grayscaleWeights.blue = data['blue'];
+    grayscaleWeights.useIntegerApproximation = data['useIntegerApproximation'];
+}
+
+function setInversionMode(inversionMode: 'original' | 'invert' | 'both') {
+    switch (inversionMode) {
+        case 'original':
+            inversionAttempts = 'dontInvert';
+            break;
+        case 'invert':
+            inversionAttempts = 'onlyInvert';
+            break;
+        case 'both':
+            inversionAttempts = 'attemptBoth';
+            break;
+        default:
+            throw new Error('Invalid inversion mode');
+    }
+}
